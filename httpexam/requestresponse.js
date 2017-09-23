886e94aa3fda926cd42c808565de4cce6c54f7dd
Park Young Kyung <ykishappy@gmail.com>
2017-09-23 16:38:38 +0900

    ejs,jade 추가

--- httpexam/requestresponse.js
+++ httpexam/requestresponse.js
@@ -1,18 +1,49 @@
 var http = require('http');
 var fs = require('fs');
+var ejs = require('ejs');
+var jade = require('jade');
 
 http.createServer(function ( request, response){
 	if (request.method == "GET"){
-		fs.readFile('index.html',function(error,data){
-			response.writeHead(200,{'Content-Type':'text/html'});
-			response.end(data);
-		});
-	}else if (request.method == 'POST'){
+		console.log(request.url +" GET");
+		if( request.url == '/'){
+			fs.readFile('index.html',function(err,data){
+				if (!err) {
+					response.writeHead(200,{'Content-Type':'text/html'});
+					response.end(data);		
+				}else{
+					response.writeHead(404); response.end();
+				}
+			});
+		}else if (request.url == '/ejs') {
+			fs.readFile('template.ejs','utf8',function(err,data){
+				if (!err) {
+					var html = ejs.render(data,
+						{name:'Park',description:'Hello, World for ejs'} )
+					response.writeHead(200,{'Content-Type':'text/html'});
+					response.end(html);
+				}
+			});
+		}else if(request.url == '/jade'){
+			fs.readFile('template.jade','utf8',function(err,data){
+				if(!err){
+					console.log('aaa')
+					var fn = jade.compile(data);
+					var html = fn(
+						{name:'Park',description:'Hello, World for jade'} )
+					response.writeHead(200,{'Content-Type':'text/html'});
+					response.end(html);
+				}
+			});
+		}
+	}
+	else if(request.method == 'POST'){
 		request.on('data',function (data) {
 			response.writeHead(200,{'Content-Type':'text/html'});
 			response.end('<h1>' + data + '</h1>');
 		});
 	}
-}).listen(52273,function(){
+}).listen(52273,function()
+{
 	console.log('Server Running at http://127.0.0.1:52273');
 });
